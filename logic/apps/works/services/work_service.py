3bd31d04030fbd0beacd813b373b10f01da11811
brianlobo <blobo@redhat.com>
2022-09-27 10:57:39 -0300

    Refactor para prepararlo para los pipelines

--- logic/apps/works/services/work_service.py
+++ logic/apps/works/services/work_service.py
@@ -10,46 +10,31 @@ from logic.apps.filesystem.services import workingdir_service
 from logic.apps.works.models.work_model import StatusFinished
 from logic.libs.logger.logger import logger
 
-_NAME_FILE_LOGS = 'logs.log'
-
-_FOLDER_MODULES = 'logic/apps/repo_modules_default'
 
 _WORKS_RUNING: Dict[str, Process] = {}
 
 
-def start(id: str, files_bytes_dict: Dict[str, bytes]):
+def exec(id: str):
 
-    logger().info(f'Generando workingdir -> proceso: {id}')
-    workingdir_service.create_by_id(id)
+    logger().info(f'Recibiendo proceso para ejecutar -> {id}')
 
     base_path = workingdir_service.fullpath(id)
 
-    for file_name in os.listdir(_FOLDER_MODULES):
+    runner_script = 'runner.pyc' if os.path.exists(
+        f'{base_path}/runner.pyc') else 'runner.py'
 
-        logger().info(f'Generando archivo -> {file_name}')
-        shutil.copy(f'{_FOLDER_MODULES}/{file_name}', base_path)
-
-    for file_name, file_bytes in files_bytes_dict.items():
-
-        logger().info(f'Generando archivo -> {file_name}')
-        with open(f'{base_path}/{file_name}', 'w') as f:
-            f.write(file_bytes.decode())
-
-    process = Process(target=_exec, args=(id,))
+    process = Process(target=_thread_exec, args=(id, runner_script))
     process.start()
 
     global _WORKS_RUNING
     _WORKS_RUNING[id] = process
 
 
-def _exec(id: str):
+def _thread_exec(id: str, runner_script: str):
 
     base_path = workingdir_service.fullpath(id)
 
-    name_file_runner_final = 'runner.pyc' if os.path.exists(
-        os.path.join(base_path, 'runner.pyc')) else 'runner.py'
-
-    cmd = f'cd {base_path} && python {name_file_runner_final}'
+    cmd = f'cd {base_path} && python {runner_script}'
 
     process = subprocess.Popen(cmd, shell=True)
     process.wait()
@@ -72,6 +57,8 @@ def delete(id: str):
 
 
 def _notify_work_end(id: str, status: StatusFinished):
+
+    logger().info(f'Proceso terminado -> {id}')
 
     url = get_var(Vars.JAIME_URL) + f'/api/v1/works/{id}/finish'
     body = {"status": status.value}
